829fd088de5dd426f0f492cd13dc65c0f80349d3
Ivan Prignano <ivanprignano@gmail.com>
2025-09-16 10:45:53 +0200

    add astro mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ivanprignano.com',
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  integrations: [mdx()]
 });
